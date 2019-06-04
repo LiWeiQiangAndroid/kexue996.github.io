b5f8a125c61127b65574fb1fd0c132f1d12e606b
liwq <lwqldsyzx@126.com>
2019-06-04 21:39:05 +0800

    kexueshangwang-996

---
+++ README.md
@@ -0,0 +1,1 @@
+该目录最终发布到 Web 服务器，或者 GitHub Pages，example: https://kexue996.github.io
---
+++ sw.js
@@ -0,0 +1,1 @@
+importScripts(__FILE__='https://cdn.jsdelivr.net/gh/etherdream/jsproxy@gh-pages/assets/bundle.b30fe48b.js')
